df87e1101d969b09e5097f283e2bedb0810deb5b
youngwoo <ds5vto@gmail.com>
2020-04-18 20:17:46 +0900

    add test case

---
+++ src/test/java/com/example/demo/SampleSpringBootTest.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class SampleSpringBootTest {
+
+	@Autowired
+	TestRestTemplate testRestTemplate;
+	
+	@MockBean
+	SampleService sampleService;
+	
+	@Test
+	public void hello() throws Exception {
+		when(sampleService.getName()).thenReturn("saelobi");
+		
+		String result = testRestTemplate.getForObject("/hello", String.class);
+		assertThat(result).isEqualTo("hello saelobi");
+	}
+	
+	
+}
--- src/test/java/com/example/demo/SpringBootTest1ApplicationTests.java
+++ src/test/java/com/example/demo/SpringBootTest1ApplicationTests.java
@@ -12,7 +12,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
